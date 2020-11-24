02995c0bfa73731a35fab26dbab44f11e4f54ad6
yermanss <germanwibaux@gmail.com>
2020-11-24 11:25:09 -0300

    Se agrega un test funcionando en el microservicio de usuario

--- Usuarios/src/test/java/com/practico/integrador/UsuarioControllerTests.java
+++ Usuarios/src/test/java/com/practico/integrador/UsuarioControllerTests.java
@@ -1,6 +1,8 @@
 package com.practico.integrador;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 /*import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -11,15 +13,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;*/
 import java.util.ArrayList;
 
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 /*import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;*/
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoJUnitRunner;
 /*import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -31,10 +35,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;*/
+import org.mockito.junit.MockitoRule;
 
-import com.practico.integrador.controller.UsuarioController;
 import com.practico.integrador.model.Usuario;
 import com.practico.integrador.repository.UsuarioRepository;
+
+import junit.framework.Assert;
+
 
 
 
@@ -43,13 +50,12 @@ import com.practico.integrador.repository.UsuarioRepository;
 
 
 @RunWith(MockitoJUnitRunner.class)
-class UsuarioRepositoryTests {
+public class UsuarioControllerTests {
 	
 	@Mock(stubOnly = true)
 	UsuarioRepository usuarioRepository;	
 	
-	@InjectMocks
-    UsuarioController userService;
+	@Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 	
 	ArrayList<Usuario> savedUsers;
 	
@@ -57,34 +63,34 @@ class UsuarioRepositoryTests {
 	
 	@Before
     public void setup() { 
-		System.out.println("paso");
-		userService.addUsuario(new Usuario((long) 999 ,"Alejandro", "21"));
-		savedUsers = new ArrayList<>();
-		Mockito.when(usuarioRepository.findAll()).then((el) -> savedUsers);
 		
 		savedUsers = new ArrayList<>();
 		users = new Usuario[] { new Usuario((long) 12345 ,"Alejandro", "21"), new Usuario((long) 123456, "Lucía", "45646")};
-		
-		Mockito.when(usuarioRepository.save(new Usuario((long) 12345 ,"Alejandro", "21"))).then((el) -> {
-            synchronized (savedUsers) {
-                savedUsers.add(el.getArgument(0));
-            }
-            return el.getArgument(0);
-        });
-		
 	}
-	 
-	@Test
-    public void testAddNullUser() {
-		userService.addUsuario(new Usuario((long) 999 ,"Alejandro", "21"));
-		try {
-			Usuario user = new Usuario((long) 156234, "otro", "123456");
-			userService.addUsuario(user);
-        } catch (IllegalArgumentException e) {
-            assertTrue(e != null);
-        }
-    }
 	
+	@Test
+	public void userEquals() {
+		System.out.print("Test one");
+		Usuario userAdd = new Usuario((long) 156234, "juan", "123456");
+		Usuario userTest = new Usuario((long) 156234, "juan", "123456");
+		assertEquals(userTest.getUsuario().toString(), userAdd.getUsuario().toString()); 
+	}
+	
+	@Test
+	public void userNotEquals() {
+		System.out.print("Test two");
+		Usuario userAdd = new Usuario((long) 156234, "pedro", "123456");
+		Usuario userTest = new Usuario((long) 156234, "manuel", "123456");
+		assertEquals(userTest.getUsuario().toString(), userAdd.getUsuario().toString()); 
+	}
+	
+	@Test
+	public void userPassCompare() {
+		System.out.print("Test three");
+		Usuario userAdd = new Usuario((long) 156234, "pedro", "123456000");
+		Usuario userTest = new Usuario((long) 156234, "manuel", "123456");
+		assertEquals(userTest.getContrasenia().toString(), userAdd.getContrasenia().toString()); 
+	}
 
 	
 
